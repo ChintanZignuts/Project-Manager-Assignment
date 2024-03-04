1fc7d998979bb3b1466fd11a89797967e730a8b6
chintanrupareliya <ckrupareliya2002@gmail.com>
2024-03-04 23:25:24 +0530

    home page created

--- src/components/FormInput.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-  <v-col cols="12" md="4" sm="6">
-    <v-text-field label="Plain" placeholder="Placeholder" variant="plain"></v-text-field>
-  </v-col>
-</template>
-<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,49 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="d-flex align-center justify-center" style="height: 100vh"></main>
+  <v-container class="d-flex flex-column align-center justify-center" fluid>
+    <div class="h-50 w-66 d-flex flex-column align-center justify-center">
+      <v-row align="center" justify="center">
+        <v-col cols="12">
+          <div class="text-center">
+            <h1>Welcome to Our Project Management Website</h1>
+            <p>This is the best place to manage your projects efficiently and effectively.</p>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row align="center" justify="center" class="flex-row">
+        <v-col cols="12">
+          <div class="text-center">
+            <VBtn color="grey" class="rounded-xl ma-5 text-black text-none" size="x-large"
+              >Sign In</VBtn
+            >
+            <VBtn color="white" class="rounded-xl text-black text-none" size="x-large"
+              >Sign Up</VBtn
+            >
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
+<style scoped>
+.v-container {
+  background: url('../assets/background2.jpg') no-repeat center center;
+  background-size: cover;
+  height: 100vh;
+  color: white;
+}
+h1 {
+  font-family: 'Arial', sans-serif;
+  font-size: 3rem;
+  color: #ffffff;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+}
+
+p {
+  font-family: 'Arial', sans-serif;
+  font-size: 1.2em;
+  color: #ffffff;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+}
+</style>
